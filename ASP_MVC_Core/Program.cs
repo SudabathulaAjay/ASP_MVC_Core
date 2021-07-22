6e8859a91c3427f9484a40ced6c636475cdfd599
Ajay Kumar Sudabathula <asudabathula@deloitte.com>
2021-07-22 16:43:57 +0530

    Testing

--- ASP_MVC_Core/Program.cs
+++ ASP_MVC_Core/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace ASP_MVC_Core
 {
@@ -18,6 +19,11 @@ namespace ASP_MVC_Core
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
