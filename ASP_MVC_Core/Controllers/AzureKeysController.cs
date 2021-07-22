e0776ba69ee03eec06ae99af28827cf7bc0c96fe
Ajay Kumar Sudabathula <asudabathula@deloitte.com>
2021-07-22 15:43:22 +0530

    test

--- ASP_MVC_Core/Controllers/AzureKeysController.cs
+++ ASP_MVC_Core/Controllers/AzureKeysController.cs
@@ -20,15 +20,10 @@ namespace ASP_MVC_Core.Controllers
         }
 
         // GET: AzureKeys
-        //public async Task<IActionResult> Index()
-        //{
-
-        //    return View(await _context.AzureKey.ToListAsync());
-        //}
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+             
+            return View(await _context.AzureKey.ToListAsync());
         }
 
         // GET: AzureKeys/Details/5
